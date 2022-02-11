f242fc84938b0f93e9036941ae91070a44a70144
Sebastian Lara <pelukdev@gmail.com>
2022-02-10 23:18:40 -0300

    Opcion de agregar actividades

--- src/components/User/Professional/DashboardProfessional.jsx
+++ src/components/User/Professional/DashboardProfessional.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Accordion, Button } from "react-bootstrap";
+import { CgProfile } from 'react-icons/cg'
 
 const DashboardProfessional = () => {
   return (
     <div>
       <div className="d-grid gap-2 mb-2">
-        <Button variant="outline-primary" onClick={() => window.location.href = "/user/profile"}>Perfil</Button>
+        <Button variant="outline-primary" onClick={() => window.location.href = "/user/profile"}><CgProfile/> Perfil</Button>
       </div>
       <Accordion defaultActiveKey="0">
         <Accordion.Item eventKey="0">
@@ -14,28 +15,7 @@ const DashboardProfessional = () => {
             <div className="d-grid gap-2">
               <Button variant="outline-primary" onClick={() => window.location.href = "/user/advisory/create"}>Crear</Button>
               <Button variant="outline-primary" onClick={() => window.location.href = "/user/advisory/update"}>Actualizar</Button>
-            </div>
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Actividad</Accordion.Header>
-          <Accordion.Body>
-            <div className="d-grid gap-2">
-              <Button variant="outline-primary" onClick={() => window.location.href = "#!"}>Crear</Button>
-              <Button variant="outline-primary" onClick={() => window.location.href = "#!"}>Ver</Button>
-              <Button variant="outline-primary" onClick={() => window.location.href = "#!"}>Actualizar</Button>
-              <Button variant="outline-primary" onClick={() => window.location.href = "#!"}>Eliminar</Button>
-            </div>
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="2">
-          <Accordion.Header>Capacitacion</Accordion.Header>
-          <Accordion.Body>
-            <div className="d-grid gap-2">
-              <Button variant="outline-primary" onClick={() => window.location.href = "#!"}>Crear</Button>
-              <Button variant="outline-primary" onClick={() => window.location.href = "#!"}>Ver</Button>
-              <Button variant="outline-primary" onClick={() => window.location.href = "#!"}>Actualizar</Button>
-              <Button variant="outline-primary" onClick={() => window.location.href = "#!"}>Eliminar</Button>
+              <Button variant="outline-primary" onClick={() => window.location.href = "/user/advisory/activity"}>Actividades</Button>
             </div>
           </Accordion.Body>
         </Accordion.Item>
