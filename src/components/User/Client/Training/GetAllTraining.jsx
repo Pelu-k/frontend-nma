d611b9e81bd2935196317d6277357613a495734a
Sebastian Lara <pelukdev@gmail.com>
2022-03-06 18:59:20 -0300

    obtener todas las capacitaciones

---
+++ src/components/User/Client/Training/GetAllTraining.jsx
@@ -0,0 +1,185 @@
+import React, { useEffect, useState } from "react";
+import {
+  Accordion,
+  Alert,
+  Badge,
+  Button,
+  Col,
+  Container,
+  Row,
+  Table,
+} from "react-bootstrap";
+import { AiOutlineEye } from "react-icons/ai";
+import Loading from "../../../Utils/Loading/Loading";
+import DashboardClient from "../DashboardClient";
+
+const GetAllTraining = () => {
+  const idUsuario = localStorage.getItem("idUsuario");
+
+  const [state, setState] = useState(true);
+  const [trainings, setTrainings] = useState([]);
+  const [activities, setActivities] = useState([]);
+
+  const URL_BASE = "http://localhost:8080/api";
+  const OPTIONS_GET = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("token"),
+    },
+  };
+
+  const changeState = () => {
+    setState(true);
+    setTimeout(() => {
+      setState(false);
+    }, 2000);
+  };
+
+  const getAllTrainingsByClientId = async () => {
+    try {
+      const res = await fetch(
+        `${URL_BASE}/trainings/client/${idUsuario}`,
+        OPTIONS_GET
+      );
+      const data = await res.json();
+      setTrainings(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getAllActivities = async () => {
+    try {
+      const res = await fetch(`${URL_BASE}/all-activities`, OPTIONS_GET);
+      const data = await res.json();
+      setActivities(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    document.title = "Actividades";
+    getAllTrainingsByClientId();
+    getAllActivities();
+    changeState();
+  }, []);
+
+  return (
+    <div className="mt-5">
+      <Container>
+        <Row>
+          <Col sm={3}>
+            <DashboardClient />
+          </Col>
+          <Col sm={9}>
+            <h2 className="text-center mb-5">Capacitaciones</h2>
+            {state ? (
+              <Loading />
+            ) : trainings.length > 0 ? (
+              <div>
+                <Accordion>
+                  {trainings.map((training, index) => (
+                    <Accordion.Item eventKey={index}>
+                      <Accordion.Header>
+                        {training.estado === "Cancelada" ? (
+                          <div>
+                            <Badge pill bg="danger">
+                              Cancelada
+                            </Badge>{" "}
+                            {training.nombre}
+                          </div>
+                        ) : training.estado === "Aprobado" ? (
+                          <div>
+                            <Badge pill bg="primary">
+                              Aprobado
+                            </Badge>{" "}
+                            {training.nombre}
+                          </div>
+                        ) : training.estado === "En aprobacion" ? (
+                          <div>
+                            <Badge pill bg="warning" text="dark">
+                              En aprobacion
+                            </Badge>{" "}
+                            {training.nombre}
+                          </div>
+                        ) : training.estado === "Finalizada" ? (
+                          <div>
+                            <Badge pill bg="success">
+                              Finalizada
+                            </Badge>{" "}
+                            {training.nombre}
+                          </div>
+                        ) : null}
+                      </Accordion.Header>
+                      <Accordion.Body>
+                        <div>
+                          <Table>
+                            <thead>
+                              <tr>
+                                <th>Razon Social</th>
+                                <th>Encargado</th>
+                                <th>Estado</th>
+                                <th></th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td>{training.razonSocial}</td>
+                                <td>{training.nombreEmpresa}</td>
+                                <td>{training.estado}</td>
+                              </tr>
+                            </tbody>
+                          </Table>
+                          <Table>
+                            <thead>
+                              <tr>
+                                <th>Nombre</th>
+                                <th>Estado</th>
+                                <th>Tipo</th>
+                                <th>Checklist</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              {activities.map((activity) =>
+                                activity.idAsesoriaFk.toString() ===
+                                training.id ? (
+                                  <tr>
+                                    <td>{activity.nombre}</td>
+                                    <td>{activity.estado}</td>
+                                    <td>{activity.tipo}</td>
+                                    <td>
+                                      <Button
+                                        variant="outline-primary"
+                                        size="sm"
+                                      >
+                                        <AiOutlineEye />
+                                        Ver
+                                      </Button>
+                                    </td>
+                                  </tr>
+                                ) : null
+                              )}
+                            </tbody>
+                          </Table>
+                        </div>
+                      </Accordion.Body>
+                    </Accordion.Item>
+                  ))}
+                </Accordion>
+              </div>
+            ) : (
+              <Alert variant="danger">
+                No se pudieron obtener las capacitaciones
+              </Alert>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default GetAllTraining;
