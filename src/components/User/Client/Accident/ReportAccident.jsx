9cfccd9f9f201dc0cc00044e50dbb0e7d5a1d059
Sebastian Lara <pelukdev@gmail.com>
2022-03-06 17:22:33 -0300

    reporte de accidentes

--- src/components/User/Client/Accident/ReportAccident.jsx
+++ src/components/User/Client/Accident/ReportAccident.jsx
@@ -1,8 +1,37 @@
-import React, { useState, useEffect } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Col, Container, FloatingLabel, Form, Row } from "react-bootstrap";
+import { IoIosSend } from "react-icons/io";
 import DashboardClient from "../DashboardClient";
 
 const ReportAccident = () => {
+  const [desc, setDesc] = useState();
+
+  const URL_BASE = "http://localhost:8080/api";
+  const OPTIONS_POST = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("token"),
+    },
+    body: JSON.stringify({
+      fecha: new Date(),
+      descripcion: desc,
+      idCliente: localStorage.getItem('idUsuario')
+    }),
+  };
+
+  const createReport = async () => {
+    try {
+      const res = await fetch(`${URL_BASE}/report-accident`, OPTIONS_POST);
+      const data = await res.text();
+      alert(data)
+      window.location.reload()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="mt-5">
       <Container>
@@ -12,6 +41,34 @@ const ReportAccident = () => {
           </Col>
           <Col sm={9}>
             <h2 className="text-center mb-5">Actividades</h2>
+            <Form>
+              <Form.Group className="mb-3">
+                <FloatingLabel label="Fecha" controlId="date">
+                  <Form.Control
+                    type="date"
+                    value={new Date().toJSON().slice(0, 10)}
+                    readOnly
+                  />
+                </FloatingLabel>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <FloatingLabel label="Descripcion" controlId="description">
+                  <Form.Control
+                    as="textarea"
+                    onChange={(e) => setDesc(e.target.value)}
+                  />
+                </FloatingLabel>
+              </Form.Group>
+              <div className="d-grid gap-2">
+                <Button
+                  variant="outline-danger"
+                  size="lg"
+                  onClick={createReport}
+                >
+                  <strong>Enviar reporte</strong> <IoIosSend />
+                </Button>
+              </div>
+            </Form>
           </Col>
         </Row>
       </Container>
